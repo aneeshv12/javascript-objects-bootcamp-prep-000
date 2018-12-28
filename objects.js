90549ebe1f79ef17982bc04fea08fd8408193c2b
Aneesh Venuturumilli <veeaneesh@gmail.com>
2018-12-28 02:16:53 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,6 +2,6 @@ var playlist = {"Kodaline":"Follow your Fire"
 };
 
 function updatePlaylist(playlist, artistName,songTitle){
-  playlist.artistName = songTitle;
+  playlist[artistName] = songTitle;
   return playlist
 }
